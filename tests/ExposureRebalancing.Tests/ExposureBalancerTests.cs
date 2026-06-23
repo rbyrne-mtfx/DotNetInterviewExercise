7b292c6dbcd67738eb7a3a04357aa00c3a3576ee
Richard Byrne <rbyrne@millenniumglobal.com>
2026-06-23 15:22:22 +0100

    Add expected outcomes

--- tests/ExposureRebalancing.Tests/ExposureBalancerTests.cs
+++ tests/ExposureRebalancing.Tests/ExposureBalancerTests.cs
@@ -1,23 +1,76 @@
 namespace ExposureRebalancing.Tests;
 
+// Scenario 1
+//           | Entity | Exposure | Capacity | Priority 
+// ----------|--------|----------|----------|---------
+// Input     | A      | 40       |    50    |    1
+//           | B      | 30       |    60    |    2
+//           | C      | 20       |    40    |    3
+//           | D      | 10       |    20    |    4
+// Expected  | A      | 50       |
+//           | B      | 60       |
+//           | C      | 0        |
+//           | D      | 0        |
+
+// Scenario 2
+//           | Entity | Exposure | Capacity | Priority 
+// ----------|--------|----------|----------|---------
+// Input     | A      | 40       |    100   |    1
+//           | B      | 30       |    60    |    2
+//           | C      | 20       |    40    |    3
+//           | D      | 10       |    20    |    4
+// Expected  | A      | 100      | 
+//           | B      | 0        | 
+//           | C      | 0        | 
+//           | D      | 0        | 
+
+// Scenario 3
+//           | Entity | Exposure | Capacity | Priority 
+// ----------|--------|----------|----------|---------
+// Input     | A      | 100      |    100   |    1
+//           | B      | 30       |    60    |    2
+//           | C      | 20       |    40    |    3
+//           | D      | 10       |    20    |    4
+// Expected  | A      | 100      |    
+//           | B      | 60       |    
+//           | C      | 0        |    
+//           | D      | 0        |    
+
+// Scenario 4
+//           | Entity | Exposure | Capacity | Priority 
+// ----------|--------|----------|----------|---------
+// Input     | A      | 100      |    100   |    1
+//           | B      | 40       |    60    |    2
+//           | C      | 20       |    40    |    3
+//           | D      | 10       |    20    |    4
+// Expected  | A      | 100      |    
+//           | B      | 60       |    
+//           | C      | 10       |    
+//           | D      | 0        |    
+
+
 [TestFixture]
 public class ExposureBalancerTests
 {
-    private List<Entity> _entities = null!;
-    private ExposureBalancer _balancer = null!;
 
-    [SetUp]
-    public void SetUp()
+    [Test]
+    public void Rebalance_Scenario_1_Successful()
     {
-        _entities =
-        [
-            new Entity("A", exposure: 40m, capacity: 50m, priority: 1),
-            new Entity("B", exposure: 30m, capacity: 60m, priority: 2),
-            new Entity("C", exposure: 20m, capacity: 40m, priority: 3),
-            new Entity("D", exposure: 10m, capacity: 20m, priority: 4),
-        ];
+    }
 
-        _balancer = new ExposureBalancer(_entities);
+    [Test]
+    public void Rebalance_Scenario_2_Successful()
+    {
+    }
+
+    [Test]
+    public void Rebalance_Scenario_3_Successful()
+    {
+    }
+
+    [Test]
+    public void Rebalance_Scenario_4_Successful()
+    {
     }
 
 }
