86df0d3e175d863bab2e379a62579c7d2a4cbc1d
Richard Byrne <rbyrne@millenniumglobal.com>
2026-06-29 11:39:04 +0100

    Add private set to Entity

--- src/ExposureRebalancing/Entity.cs
+++ src/ExposureRebalancing/Entity.cs
@@ -3,7 +3,7 @@ namespace ExposureRebalancing;
 public class Entity
 {
     public string EntityId { get; }
-    public decimal Exposure { get; }
+    public decimal Exposure { get; private set; }
     public decimal Capacity { get; }
     public int Priority { get; }
     public Entity(string entityId, decimal exposure, decimal capacity, int priority)
